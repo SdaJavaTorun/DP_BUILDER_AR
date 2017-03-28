38346118bec7b5da7192ebd27c08c5abb4362f54
grovas <arzcir@gmail.com>
2017-03-28 19:37:30 +0200

    3rd commit

--- src/com/dpBuilder/Main.java
+++ src/com/dpBuilder/Main.java
@@ -10,14 +10,18 @@ public class Main {
         Builder tesla3 = new Tesla3();
 
         director.setBuilder(tesla);
-        director.setBuilder(tesla2);
-        director.setBuilder(tesla3);
-
         director.build();
-
         Carset carset = director.getCarset();
-
         System.out.println(carset.toString());
 
+        director.setBuilder(tesla2);
+        director.build();
+        Carset carset2 = director.getCarset();
+        System.out.println(carset2.toString());
+
+        director.setBuilder(tesla3);
+        director.build();
+        Carset carset3 = director.getCarset();
+        System.out.println(carset3.toString());
     }
 }
